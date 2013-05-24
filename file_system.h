7f89c46b025860cbfdc99f80fc510851fe8d9c11
nicholashaydensmith <thesmithdynasty@gmail.com>
2013-05-23 18:22:21 -0600

    Update file_system.h

--- file_system.h
+++ file_system.h
@@ -1,3 +1,12 @@
+#ifndef __FILE_SYSTEM_H__
+#define __FILE_SYSTEM_H__
+
+#include "mydisk.h"
+
+#define SUPERBLOCK 0
+#define ROOTBLOCK 1
+#define FREEBLOCK 2
+
 struct Inode{
   int size;
   int ** pointers;
@@ -23,3 +32,5 @@ void read_root_dir(disk_t disk);
 void write_block_map(disk_t disk);
 
 void read_block_map(disk_t disk);
+
+#endif
