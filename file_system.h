0912c51039c7d7456e1f40d9138c4680ae4675b6
remosewa <risker259@gmail.com>
2013-05-25 19:10:36 -0600

    Update file_system.h

--- file_system.h
+++ file_system.h
@@ -7,19 +7,25 @@
 #define ROOTBLOCK 1
 #define FREEBLOCK 2
 
-struct Inode{
+typedef struct Inode{
   int size;
   int ** pointers;
   int nextInodePointer;
-};
+} Inode;
 
-typedef struct Inode *Inode;
+Inode* readInode(disk_t disk, int block);
 
-Inode readInode(int block);
+void writeInode(disk_t disk, int block, Inode* inode);
 
-void writeInode(int block, Inode inode);
+// Inodes which represent directories should be size = 0, pointers will be to other inodes
+// the max # of pointers is (BLOCK_SIZE-ceil(log10(disk_size)*2)/(ceil(log10(disk_size))+1) 
+// if you have more pointers than that, then it is your job to create an Inode with the remaining
+// pointers, write the inode, and then give the block location of that inode to the previous Inode.
+// Additional inodes should have size = to the number of blocks in them
+Inode* createInode(int size, int ** pointers, int * block);
 
-Inode createInode(int size, int ** pointers);
+//do not write inode after freeing
+void freeInode(Inode * inode);
 
 void write_super_block(disk_t disk);
 
