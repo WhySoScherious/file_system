f7e150ce6b0cfd8585a195f2326435851b9ad350
remosewa <risker259@gmail.com>
2013-05-27 19:49:03 -0600

    Update file_system.h
    added functions for removing and adding pointers. This will abstract some of the details of Inodes from the user.

--- file_system.h
+++ file_system.h
@@ -28,9 +28,9 @@ Inode* readInode(disk_t disk, int block);
 //Inode must be freed.
 Inode* writeInode(disk_t disk, int block, int * pointers,bool directory);
 
-InodeRemovePointer(disk_t disk, Inode * inode, int block_pointer);
+void inodeRemovePointer(disk_t disk, Inode * inode, int block_pointer);
 
-InodeAddPointer(disk_t disk, Inode * inode, int block_pointer);
+void inodeAddPointer(disk_t disk, Inode * inode, int block_pointer);
  
 //will safely remove Inode and all pointers;
 void deleteInode(disk_t disk, int block);
