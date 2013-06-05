477b39280becca6a67b384510af118ffd603c81e
nicholashaydensmith <thesmithdynasty@gmail.com>
2013-06-05 13:45:52 -0600

    Update myformat.c

--- myformat.c
+++ myformat.c
@@ -13,8 +13,11 @@ int main(int argc, char** argv){
    char *disk_name = argv[1];
    assert(disk_name);
    disk_t disk = opendisk(disk_name);
+   int blockmap[disk->size];
+   blockmap[0] = blockmap[1] = blockmap[2] = 1;
    write_super_block(disk);
    write_root_dir(disk);
-   write_block_map(disk);
+   write_block_map(disk,&blockmap);
    return 0;
 }
+
